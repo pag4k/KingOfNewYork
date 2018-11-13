a7708b78a19d1afd2c56d72116328838de55db78
Pierre-André Gagnon <pagagnon@gmail.com>
2018-11-13 12:33:30 -0500

    Finished strategy pattern first pass.

---
+++ rolldicestrategy.cpp
@@ -0,0 +1,20 @@
+#include "rolldicestrategy.h"
+
+
+namespace KingOfNewYork
+{
+    FDiceResult HumanRollDice::RollDice(FDiceRoller &DiceRoller, const int DiceCount, const int RollCount)
+    {
+
+    }
+
+    FDiceResult AggressiveRollDice::RollDice(FDiceRoller &DiceRoller, const int DiceCount, const int RollCount)
+    {
+
+    }
+
+    FDiceResult ModerateRollDice::RollDice(FDiceRoller &DiceRoller, const int DiceCount, const int RollCount)
+    {
+
+    }
+}
